eff376322b27aaace831bd60e25a02561e2ea8f9
xixi222 <761280449@qq.com>
2023-04-03 13:06:16 +0800

    thankY

--- src/threets/review/gltf.ts
+++ src/threets/review/gltf.ts
@@ -15,53 +15,58 @@ gltfLoader.load(new URL("@/assets/box.glb", import.meta.url).href, (gltf) => {
 			bufArrays.push(array);
 		}
 	});
-});
-gltfLoader.load(new URL("@/assets/box1.glb", import.meta.url).href, (gltf) => {
-	gltf.scene.traverse((child: any) => {
-		console.log(child.geometry);
+	gltfLoader.load(
+		new URL("@/assets/box1.glb", import.meta.url).href,
+		(gltf) => {
+			gltf.scene.traverse((child: any) => {
+				console.log(child.geometry);
 
-		if (child.isMesh) {
-			child.geometry.scale(0.5, 0.5, 0.5);
-			child.geometry.translate(1.7, 0.1, 0);
+				if (child.isMesh) {
+					child.geometry.scale(0.5, 0.5, 0.5);
+					child.geometry.translate(1.7, 0.1, 0);
 
-			// child.geometry.position.y = -0.15;
-			const { array } = child.geometry.attributes.position;
-			bufArrays.push(array);
+					// child.geometry.position.y = -0.15;
+					const { array } = child.geometry.attributes.position;
+					bufArrays.push(array);
+				}
+			});
+			gltfLoader.load(
+				new URL("@/assets/qq1.glb", import.meta.url).href,
+				(gltf) => {
+					gltf.scene.traverse((child: any) => {
+						if (child.isMesh) {
+							// child.geometry.translate(-1.4, 0.1, 0);
+							child.geometry.scale(0.8, 0.8, 0.8);
+							console.log(child.geometry);
+							child.geometry.rotateX(Math.PI / 2);
+							// child.geometry.rotateZ(Math.PI);
+							child.geometry.translate(0, 0, 4);
+
+							// child.geometry.rotateZ = Math.PI / 2;
+							// child.geometry.rotateY = Math.PI / 2;
+
+							const { array } = child.geometry.attributes.position;
+							bufArrays.push(array);
+						}
+					});
+					gltfLoader.load(
+						new URL("@/assets/sphere.glb", import.meta.url).href,
+						(gltf) => {
+							gltf.scene.traverse((child: any) => {
+								if (child.isMesh) {
+									child.geometry.translate(-1.4, 0.1, 0);
+									child.geometry.scale(1.2, 1.2, 1.2);
+
+									const { array } = child.geometry.attributes.position;
+									bufArrays.push(array);
+								}
+							});
+						}
+					);
+				}
+			);
 		}
-	});
+	);
 });
 
-gltfLoader.load(new URL("@/assets/qq1.glb", import.meta.url).href, (gltf) => {
-	gltf.scene.traverse((child: any) => {
-		if (child.isMesh) {
-			// child.geometry.translate(-1.4, 0.1, 0);
-			child.geometry.scale(0.8, 0.8, 0.8);
-			console.log(child.geometry);
-			child.geometry.rotateX(Math.PI / 2);
-			// child.geometry.rotateZ(Math.PI);
-			child.geometry.translate(0, 0, 4);
-
-			// child.geometry.rotateZ = Math.PI / 2;
-			// child.geometry.rotateY = Math.PI / 2;
-
-			const { array } = child.geometry.attributes.position;
-			bufArrays.push(array);
-		}
-	});
-});
-
-gltfLoader.load(
-	new URL("@/assets/sphere.glb", import.meta.url).href,
-	(gltf) => {
-		gltf.scene.traverse((child: any) => {
-			if (child.isMesh) {
-				child.geometry.translate(-1.4, 0.1, 0);
-				child.geometry.scale(1.2, 1.2, 1.2);
-
-				const { array } = child.geometry.attributes.position;
-				bufArrays.push(array);
-			}
-		});
-	}
-);
 export { bufArrays, manager };
